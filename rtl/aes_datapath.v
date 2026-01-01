ac6facd887daf6a8b3c3637a4869ad340b1eec8d
Lavear <ismaillwarsi@gmail.com>
2026-01-02 02:29:46 +0530

    Implementation of AES Datapath for mathematical operations

--- rtl/aes_datapath.v
+++ rtl/aes_datapath.v
@@ -1,9 +1,36 @@
 module aes_datapath(
-    input clk, 
-    input rst_n,
-    input final_round,
-    input [127:0]round_key,
     input [127:0]state_in,
+    input [127:0]round_key,
+    input final_round,
     output [127:0]state_out
 );
+
+wire [127:0]subbytes_out;
+wire [127:0]shiftrows_out;
+wire [127:0]mixcolumns_out;
+wire [127:0]addroundkey_in;
+
+subbytes u_subbytes(
+    .in_state(state_in),
+    .out_state(subbytes_out)
+);
+
+shiftrows u_shiftrows(
+    .in_state(subbytes_out),
+    .out_state(shiftrows_out)
+);
+
+mixcolumns u_mixcolumns(
+    .in_state(shiftrows_out),
+    .out_state(mixcolumns_out)
+);
+
+assign addroundkey_in = final_round ? shiftrows_out : mixcolumns_out;
+
+addroundkey u_addroundkey(
+    .in_state(addroundkey_in),
+    .roundkey(round_key),
+    .out_state(state_out)
+);
+
 endmodule
